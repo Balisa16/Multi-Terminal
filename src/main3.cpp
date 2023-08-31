512a76eef6821c0e64d3fd8473768bcf41c89bd7
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-08-31 09:31:50 +0700

    Add client

--- src/main3.cpp
+++ src/main3.cpp
@@ -2,13 +2,34 @@
 #include <vte/vte.h>
 #include <iostream>
 #include <string>
+#include <termios.h>
+#include <unistd.h>
 
 struct TerminalPair{
     GtkWidget* terminal;
     const char* command;
+    const char* client;
     int status;
-    TerminalPair(GtkWidget* term, const char *cmd, int stat): terminal(term), command(cmd), status(stat){}
+    TerminalPair(GtkWidget* term, const char *cmd, const char *cl, int stat): terminal(term), command(cmd), client(cl), status(stat){}
 };
+
+std::string get_password() {
+    std::string password;
+    termios oldt, newt;
+    
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    
+    std::cout << "Enter client password: ";
+    std::cin >> password;
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    std::cout << std::endl;
+    
+    return password;
+}
 
 TerminalPair* cast_data(gpointer data)
 {
@@ -16,7 +37,6 @@ TerminalPair* cast_data(gpointer data)
 }
 
 static void prepare_button_clicked(GtkButton* button, gpointer user_data) {
-
     TerminalPair* data = cast_data(user_data);
 
     // Feed the command to the terminal
@@ -24,14 +44,22 @@ static void prepare_button_clicked(GtkButton* button, gpointer user_data) {
     vte_terminal_feed_child(VTE_TERMINAL(data->terminal), "\n", -1);
 }
 
+static void run_button_clicked(GtkButton* button, gpointer user_data) {
+    TerminalPair* data = cast_data(user_data);
+    vte_terminal_feed_child(VTE_TERMINAL(data->terminal), data->client, -1);
+    vte_terminal_feed_child(VTE_TERMINAL(data->terminal), "\n", -1);
+}
 
 static GtkWidget* create_terminal(GtkWidget* container) {
     GtkWidget* terminal = vte_terminal_new();
     vte_terminal_set_cursor_blink_mode(VTE_TERMINAL(terminal), VTE_CURSOR_BLINK_OFF);
 
     const char* command = "/usr/bin/bash";
-    char** commandArray = new char*[2];
 
+    const char* homeDirectory = getenv("HOME");
+
+    // Construct the command array
+    char** commandArray = new char*[2];
     commandArray[0] = new char[strlen(command) + 1];
     strcpy(commandArray[0], command);
 
@@ -39,26 +67,25 @@ static GtkWidget* create_terminal(GtkWidget* container) {
 
     vte_terminal_spawn_sync(VTE_TERMINAL(terminal),
                             VTE_PTY_DEFAULT,
-                            NULL,
+                            homeDirectory,
                             commandArray,
                             NULL, G_SPAWN_DEFAULT, NULL,
                             NULL, NULL,
                             NULL, NULL
                             );
     gtk_container_add(GTK_CONTAINER(container), terminal);
+
     // Clean up memory
-    delete[] commandArray[0];
+    for (int i = 0; commandArray[i] != nullptr; ++i) {
+        delete[] commandArray[i];
+    }
     delete[] commandArray;
+
     return terminal;
 }
 
-static void run_button_clicked(GtkButton* button, gpointer user_data) {
-    // Handle the "Run" button click
-    std::cout << "Run button clicked : " << user_data << std::endl;
-}
 
-
-static void create_terminal_with_buttons(GtkWidget* container, const char *cmd) {
+static void create_terminal_with_buttons(GtkWidget* container, const char *cmd, const char *client) {
     GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_container_add(GTK_CONTAINER(container), box);
 
@@ -71,7 +98,7 @@ static void create_terminal_with_buttons(GtkWidget* container, const char *cmd) 
     GtkWidget* prepare_button = gtk_button_new_with_label("Connect");
     GtkWidget* run_button = gtk_button_new_with_label("Run");
 
-    TerminalPair* terminal_pair = new TerminalPair(terminal_widget, cmd, -1);
+    TerminalPair* terminal_pair = new TerminalPair(terminal_widget, cmd, client, -1);
 
     g_signal_connect(prepare_button, "clicked", G_CALLBACK(prepare_button_clicked), (gpointer)terminal_pair);
     g_signal_connect(run_button, "clicked", G_CALLBACK(run_button_clicked), (gpointer)terminal_pair);
@@ -95,6 +122,17 @@ int main(int argc, char* argv[]) {
         "rosservice call /mavros/set_stream_rate 0 100 1",
         "rosrun emiro 1 0 1"
     };
+
+    if(argc < 2)
+        std::cout << "Use command ./<program> <client_addr>" << std::endl;
+    
+    std::string client_addr = std::string(argv[1]);
+    std::cout << client_addr;
+    std::string password = get_password();
+    std::cout << "Password entered: " << password << std::endl;
+
+    std::string client_args = "sshpass -p '" + password + "' ssh -t " + client_addr + " 'lsb_release -a  && exec $SHELL'";
+
 
     gtk_init(&argc, &argv);
 
@@ -122,7 +160,7 @@ int main(int argc, char* argv[]) {
         
         gtk_container_add(GTK_CONTAINER(terminal_frame), terminal_box);
         
-        create_terminal_with_buttons(terminal_box, command[i].c_str());
+        create_terminal_with_buttons(terminal_box, command[i].c_str(), client_args.c_str());
         
         gtk_box_pack_start(GTK_BOX(upper_box), terminal_frame, TRUE, TRUE, 5);
     }
@@ -137,7 +175,7 @@ int main(int argc, char* argv[]) {
         
         gtk_container_add(GTK_CONTAINER(terminal_frame), terminal_box);
         
-        create_terminal_with_buttons(terminal_box, command[i].c_str());
+        create_terminal_with_buttons(terminal_box, command[i].c_str(), client_args.c_str());
         
         gtk_box_pack_start(GTK_BOX(lower_box), terminal_frame, TRUE, TRUE, 5);
     }
